b2de588c1fea206f06d30db52f39b4709532774b
İlhan Atahan <80551220+ilhan-athn7@users.noreply.github.com>
2022-09-04 11:55:39 +0300

    Revert recent commit
    Needed in case of more then one PID (process id) returns

--- GI_affinity_workaround/Genshin_start.sh
+++ GI_affinity_workaround/Genshin_start.sh
@@ -2,7 +2,7 @@ am start -n com.miHoYo.GenshinImpact/com.miHoYo.GetMobileInfo.MainActivity
 
 sleep 15
 
-GI_pid="$(pidof com.miHoYo.GenshinImpact)"
+GI_pid="$(echo `pidof com.miHoYo.GenshinImpact`)"
 
 for limit_core in $GI_pid;
 do taskset -ap 1 $limit_core;
